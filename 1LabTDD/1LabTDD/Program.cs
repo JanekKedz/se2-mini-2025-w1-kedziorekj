2ad17d43461cf69bb20d10bfd413bc7882231a6d
JanekKedz <01161076@pw.edu.pl>
2025-03-11 09:02:35 +0100

    Add files via upload

---
+++ 1LabTDD/1LabTDD/Program.cs
@@ -0,0 +1,63 @@
+﻿using System.Diagnostics;
+using static _1LabTDD.TestClass;
+
+namespace _1LabTDD
+{
+    internal class Program
+    {
+        public class CalcClass
+        {
+            public CalcClass() { }
+
+            public int Calc(string s)
+            {
+                IEnumerable<Char> allowedChars = new[] { '0', '1', '2', '3', '4', '5', '6', '7', '8', '9', '-', ',', '\n' };
+                String result = new(
+                  s.Select(x => !allowedChars.Contains(x) ? ' ' : x).ToArray()
+                );
+                int sum = 0;
+
+                if (result.Length == 0)
+                    sum = 0;
+                else if (result.Count(f => f == ',' || f == '\n') == 0)
+                {
+                    isNegative(result);
+                    if (moreThanTousand(result))
+                        sum = 0;
+                    sum = Int32.Parse(s);
+                }
+                else if (result.Count(f => f == ',' || f == '\n') > 0)
+                {
+                    string[] arr = Array.ConvertAll(result.Split([',', '\n']), p => p.Trim());
+                    foreach (string str in arr)
+                    {
+                        isNegative(str);
+                        if (!moreThanTousand(str))
+                            sum += Int32.Parse(str);
+                    }
+                }
+                return sum;
+            }
+
+            private void isNegative(string s)
+            {
+                if (s.Length > 1)
+                    if (Int32.Parse(s) < 0)
+                        throw new ArgumentException();
+            }
+
+            private bool moreThanTousand(string s)
+            {
+                if (s.Length > 3)
+                    if (Int32.Parse(s) > 1000)
+                        return true;
+                return false;
+            }
+        }
+        static void Main(string[] args)
+        {
+            CalcClassTest test = new();
+            test.CalcTest();
+        }
+    }
+}
---
+++ 1LabTDD/1LabTDD/TestClass.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using static _1LabTDD.Program;
+using static System.Runtime.InteropServices.JavaScript.JSType;
+
+namespace _1LabTDD
+{
+    class TestClass
+    {
+        public class CalcClassTest
+        {
+            public CalcClassTest() { }
+
+            public void CalcTest()
+            {
+
+                //Given
+                string s1 = "", s2 = "0", s3 = "32,4", s4 = "2\n21", s5 = "2,4\n5", s6 = "-5", s7 = "500,1025", s8 = "//40,6", s9 = "//[50,60]", s10 = "//[500],[250]";
+                CalcClass cc = new CalcClass();
+
+                //When
+
+                //1 Lenght of input is 0, return 0
+                //2 Input is a number, return the number
+                //3 Input is two numbers separated with coma, return the sum
+                //4 Input is two numbers separated with newLine, return the sum
+                //5 Input is numbers separated with both comas and/or newLines, return the sum
+                //6 Input contains a negative number, throw ArgumentException
+                //7 Input contains numbers bigger than 1000, ignore them
+                //8,9,10 Input contains slashes at the beginning and brackets in different configurations? I am sorry I do not remember it exactly.
+
+                //Then
+                Debug.Assert(Equals(cc.Calc(s1), 0));
+                Debug.Assert(Equals(cc.Calc(s2), 0));
+                Debug.Assert(Equals(cc.Calc(s3), 36));
+                Debug.Assert(Equals(cc.Calc(s4), 23));
+                Debug.Assert(Equals(cc.Calc(s5), 11));
+
+                bool exceptionThrown = false;
+                try
+                {
+                    cc.Calc(s6);
+                }
+                catch (ArgumentException)
+                {
+                    exceptionThrown = true;
+                }
+                Debug.Assert(exceptionThrown);
+
+                Debug.Assert(Equals(cc.Calc(s7), 500));
+                Debug.Assert(Equals(cc.Calc(s8), 46));
+                Debug.Assert(Equals(cc.Calc(s9), 110));
+                Debug.Assert(Equals(cc.Calc(s10), 750));
+            }
+        }
+    }
+}
+
